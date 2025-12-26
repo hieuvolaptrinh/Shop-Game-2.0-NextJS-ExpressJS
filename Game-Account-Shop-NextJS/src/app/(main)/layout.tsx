2739c1582acb26fc48dc8bb029b30b06f2f74435
nguyen dang viet <vietnguyen.1022005@gmail.com>
2025-12-26 23:27:37 +0700

    xóa hết folder cũ

--- Game-Account-Shop-NextJS/src/app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import LayoutDefault from "@/layouts/layout.default";
-
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <LayoutDefault>{children}</LayoutDefault>;
-}
--- Game-Account-Shop-NextJS/src/utils/contact.info.ts
+++
@@ -1,1 +0,0 @@
-export const DISCORD_CONTACT_LINK = "https://discord.gg/8DrYCxTV7u";
